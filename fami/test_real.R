d05ab1b24559ba4ab69bed72506cb3336c9e4c5f
zminmin <zhao_925437482@sjtu.edu.cn>
2020-03-19 19:18:41 -0500

    Update test_real.R

--- fami/test_real.R
+++ fami/test_real.R
@@ -13,6 +13,51 @@ centering <- function(X, Y, e1, e2, e3, f1, f2, f3){
 
   forest.e3 <- regression_forest(X, e3)
   e3.hat = predict(forest.e3)$predictions
+
+  forest.f1 <- regression_forest(X, f1)
+  f1.hat = predict(forest.f1)$predictions
+
+  forest.f2 <- regression_forest(X, f2)
+  f2.hat = predict(forest.f2)$predictions
+
+  forest.f3 <- regression_forest(X, f3)
+  f3.hat = predict(forest.f3)$predictions
+
+
+  data <- cbind(X, Y = Y - Y.hat,
+                e1 = e1 - e1.hat, e2 = e2 - e2.hat, e3 = e3 - e3.hat,
+                f1 = f1 - f1.hat, f2 = f2 - f2.hat, f3 = f3 - f3.hat)
+
+  return(data)
+}
+
+n <- 3000
+x_number <- 6
+dt <- rbinom(n * x_number, 1, 0.5)
+dim(dt) <- c(n, x_number)
+dt <- data.frame(dt)
+names(dt) <- c(paste0('X', 1:x_number))
+
+dt$e1 <- runif(n, min = 1, max = 1000)
+dt$e2 <- runif(n, min = 1, max = 1000)
+dt$e3 <- runif(n, min = 1, max = 1000)
+dt$f1 <- runif(n, min = 1, max = 1000)
+dt$f2 <- runif(n, min = 1, max = 1000)
+dt$f3 <- runif(n, min = 1, max = 1000)
+
+dt$Y <- dt$X1 + dt$X2 + dt$X3 + dt$X4 + dt$X5 + dt$X6 +
+  dt$e1 + dt$e2 + dt$e3 +
+  dt$f1 +
+  (dt$X1) * dt$f2 +
+  (dt$X2 + dt$X3) *dt$f3 +
+  rnorm(n, mean = 0, sd = .1)
+
+dt <- centering(dt[,1:x_number], dt$Y, dt$e1, dt$e2, dt$e3, dt$f1, dt$f2, dt$f3)
+
+
+
+#X <- dt[,1:441]
+X <- dt[,1:6]
 Y <- dt$Y
 e1 <- dt$e1
 e2 <- dt$e2
